e585260836b0f2e0121cc42f0f166e194fe03cb5
Jurian Sluiman <jurian@soflomo.com>
2011-11-27 12:53:16 +0100

    Add closure to hide show() method and change return false to preventDefault()

--- src/confirmation.js
+++ src/confirmation.js
@@ -1,19 +1,20 @@
-$.fn.confirmation = function(o){
-    jQuery(this).click(function() {
-        // e element clicked, t template, o options from user
-        var e = jQuery(this), t = e.data('template');
-        if (t !== undefined) {
-            if ('#' === t.charAt(0)) {
-                show(e, jQuery(t), o);
-            } else {
-                $.get(t, function (data) {
-                    show(e, jQuery('<div>').html(data), o);
-                });
+(function($) {
+    $.fn.confirmation = function(o){
+        jQuery(this).click(function(event) {
+            event.preventDefault();
+            // e element clicked, t template, o options from user
+            var e = jQuery(this), t = e.data('template');
+            if (t !== undefined) {
+                if ('#' === t.charAt(0)) {
+                    show(e, jQuery(t), o);
+                } else {
+                    $.get(t, function (data) {
+                        show(e, jQuery('<div>').html(data), o);
+                    });
+                }
             }
-            
-            return false;
-        }
-    });
+        });
+    };
     
     function show (e, t, o) {
         var dialog = $.extend({}, o), buttons = {}, method  = 'GET',
@@ -31,7 +32,7 @@ $.fn.confirmation = function(o){
         if (e.data('title') !== undefined) {
             title = e.data('title');
         }
-        
+
         buttons[submit] = function (){
             $.ajax(e.attr('href'), {
                 type: method
@@ -41,7 +42,7 @@ $.fn.confirmation = function(o){
         buttons[cancel] = function (){
             t.dialog('close');
         };
-        
+
         var changed = false, html = t.html();
         for (var name in e.data()) {
             var val = e.data(name), key = '%'+name+'%';
@@ -54,9 +55,9 @@ $.fn.confirmation = function(o){
         if (changed) {
             t.html(html);
         }
-        
+
         dialog.title   = title;
         dialog.buttons = buttons;
         t.dialog(dialog);
-    }
-};
+    };
+})(jQuery);
